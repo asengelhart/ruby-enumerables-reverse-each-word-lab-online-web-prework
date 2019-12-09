b116d05d408885ac5c813c1246e8955d38048796
Alex Engelhart <asengelhart@yahoo.com>
2019-12-09 01:47:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,11 @@ def reverse_each_word(source)
   result = []
   source_array = source.split(" ")
   source_array.each {|word| result << word.reverse}
-  result.reduce(""){|memo, word| memo + " " + word}
+  result.reduce("") do |memo, word| 
+    if memo != ""
+      memo += " "
+    end 
+    memo += word 
+    memo
+  end 
 end 
